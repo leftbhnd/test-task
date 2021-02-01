a5013a5d527461753d5a54d51347a872557b7543
leftbhnd <laletindima@gmail.com>
2021-02-01 22:12:13 +0500

    feat: добавление строки поиска для фильтра элементов

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     Тестовое здание
+    <div>
+      <input v-model="findUser">
+    </div>
     <table>
       <thead>
         <tr>
@@ -10,18 +13,18 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="(item) in getData" :key="item.id">
-          <td><input v-model="item.name" :disabled="item.isDisable"/></td>
-          <td><input v-model="item.age" :disabled="item.isDisable"/></td>
-          <td><input v-model="item.address" :disabled="item.isDisable"/></td>
-          <td><button v-show="item.isDisable" @click="change(item.id)">Change</button></td>
-          <td><button v-show="!item.isDisable" @click="save(item.id)">Save</button></td>
-          <td><button @click="remove(item.id)">Delete</button></td>
+        <tr v-for="(user) in filtredUsers" :key="user.id">
+          <td><input v-model="user.name" :disabled="user.isDisable"/></td>
+          <td><input v-model="user.age" :disabled="user.isDisable"/></td>
+          <td><input v-model="user.address" :disabled="user.isDisable"/></td>
+          <td><button v-show="user.isDisable" @click="change(user.id)">Change</button></td>
+          <td><button v-show="!user.isDisable" @click="save(user.id)">Save</button></td>
+          <td><button @click="remove(user.id)">Delete</button></td>
         </tr>
         <tr>
-          <td><input v-model="newItemName" type="text"></td>
-          <td><input v-model="newItemAge" type="text"></td>
-          <td><input v-model="newItemAddress" type="text"></td>
+          <td><input v-model="newUserName" type="text"></td>
+          <td><input v-model="newUserAge" type="text"></td>
+          <td><input v-model="newUserAddress" type="text"></td>
           <td><button @click="add">Add Item</button></td>
         </tr>
       </tbody>
@@ -36,34 +39,47 @@ export default {
   name: 'app',
   data () {
     return {
-      newItemName: '',
-      newItemAge: '',
-      newItemAddress: ''
+      newUserName: '',
+      newUserAge: '',
+      newUserAddress: '',
+      findUser: ''
     }
   },
   computed: {
-    ...mapGetters(['getData'])
+    ...mapGetters(['getUsers']),
+    filtredUsers: function () {
+      if (this.findUser) {
+        return this.getUsers.filter(el => {
+          if (el.name.includes(this.findUser)) {
+            return el.name.includes(this.findUser)
+          }
+          if (el.address.includes(this.findUser)) {
+            return el.address.includes(this.findUser)
+          }
+        })
+      } else return this.getUsers
+    }
   },
   methods: {
-    ...mapActions(['changeItem', 'saveItem', 'addItem', 'removeItem']),
+    ...mapActions(['changeUser', 'saveUser', 'addUser', 'removeUser']),
     change: function (id) {
-      this.$store.dispatch('changeItem', id)
+      this.$store.dispatch('changeUser', id)
     },
     save: function (id) {
-      this.$store.dispatch('saveItem', id)
+      this.$store.dispatch('saveUser', id)
     },
     add: function () {
-      this.$store.dispatch('addItem', {
-        name: this.newItemName,
-        age: this.newItemAge,
-        address: this.newItemAddress
+      this.$store.dispatch('addUser', {
+        name: this.newUserName,
+        age: this.newUserAge,
+        address: this.newUserAddress
       })
-      this.newItemName = ''
-      this.newItemAge = ''
-      this.newItemAddress = ''
+      this.newUserName = ''
+      this.newUserAge = ''
+      this.newUserAddress = ''
     },
     remove: function (id) {
-      this.$store.dispatch('removeItem', { id: id })
+      this.$store.dispatch('removeUser', { id: id })
     }
   }
 }
