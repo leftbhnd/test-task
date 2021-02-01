173314226bdd89bf67d4be0a8d830c9f803081f1
leftbhnd <laletindima@gmail.com>
2021-02-01 19:27:21 +0500

    feat: добавил работу с api сервера, добавил ts

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,9 +4,29 @@ import fetch from 'node-fetch'
 
 Vue.use(Vuex)
 
+interface DataType {
+  name: string;
+  age: number;
+  address: string;
+  id: number;
+  isDisable: boolean;
+}
+
+interface Response {
+  data: {
+    name: string;
+    age: number;
+    address: string;
+    id: number;
+    isDisable: boolean;
+  };
+  message: string;
+  id: number;
+}
+
 export default new Vuex.Store({
   state: {
-    data: []
+    data: [] as Array<DataType>
   },
   mutations: {
     GET_DATA: function (state, payload) {
@@ -19,7 +39,10 @@ export default new Vuex.Store({
       state.data = payload
     },
     ADD_ITEM: function (state, payload) {
-      state.data.push(payload)
+      state.data = payload
+    },
+    REMOVE_ITEM: function (state, payload) {
+      state.data = payload
     }
   },
   actions: {
@@ -27,7 +50,7 @@ export default new Vuex.Store({
       try {
         const response = await fetch('http://localhost:8118/api/users')
         let payload = await response.json()
-        payload = payload.data.map((el: any) => {
+        payload = payload.data.map((el: DataType) => {
           el.isDisable = true
           return el
         })
@@ -38,7 +61,7 @@ export default new Vuex.Store({
     },
     changeItem: function ({ commit }, payload) {
       const newState = [...this.state.data]
-      const changedState = newState.map((el: any) => {
+      const changedState = newState.map((el: DataType) => {
         if (el.id === payload) {
           el.isDisable = false
           return el
@@ -47,19 +70,76 @@ export default new Vuex.Store({
       payload = changedState
       commit('CHANGE_ITEM', payload)
     },
-    saveItem: function ({ commit }, payload) {
+    saveItem: async function ({ commit }, payload) {
       const newState = [...this.state.data]
-      const changedState = newState.map((el: any) => {
-        if (el.id === payload) {
-          el.isDisable = true
-          return el
-        } else return el
-      })
-      payload = changedState
-      commit('SAVE_ITEM', payload)
+      const changedItem = newState.find((el: DataType) => el.id === payload) as DataType
+      try {
+        const response = await fetch('http://localhost:8118/api/update', {
+          method: 'PATCH',
+          body: JSON.stringify(changedItem),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        })
+        const result = await response.json() as Response
+        const user = result.data
+        const changedState = newState.map((el: DataType) => {
+          if (el.id === user.id) {
+            el.name = user.name
+            el.age = user.age
+            el.address = user.address
+            el.isDisable = true
+            return el
+          } else return el
+        })
+        payload = changedState
+        commit('SAVE_ITEM', payload)
+      } catch (error) {
+        console.error(error)
+      }
     },
-    addItem: function ({ commit }, payload) {
-      commit('ADD_ITEM', payload)
+    addItem: async function ({ commit }, payload) {
+      const newState = [...this.state.data]
+      try {
+        const response = await fetch('http://localhost:8118/api/user', {
+          method: 'POST',
+          body: JSON.stringify(payload),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        })
+        const result = await response.json() as Response
+        let user = {} as DataType
+        user = result.data
+        user.isDisable = true
+        newState.push(user)
+        payload = newState
+        commit('ADD_ITEM', payload)
+      } catch (error) {
+        console.error(error)
+      }
+    },
+    removeItem: async function ({ commit }, payload) {
+      const newState = [...this.state.data]
+      try {
+        const response = await fetch('http://localhost:8118/api/delete', {
+          method: 'DELETE',
+          body: JSON.stringify(payload),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        })
+        const result = await response.json() as Response
+        const id = result.id
+        const message = result.message
+        if (message === 'deleted') {
+          const changedState = newState.filter((el: DataType) => el.id !== id) as Array<DataType>
+          payload = changedState
+        }
+        commit('REMOVE_ITEM', payload)
+      } catch (error) {
+        console.error(error)
+      }
     }
   },
   getters: {
