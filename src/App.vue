e111a1078188cb033d210d62e4af5ce8deaa3cdf
leftbhnd <laletindima@gmail.com>
2021-02-02 11:13:00 +0500

    refactor: переименование переменной

--- src/App.vue
+++ src/App.vue
@@ -2,29 +2,37 @@
   <div>
     Тестовое здание
     <div>
-      <input v-model="findUser">
+      <input v-model="findUser" />
     </div>
     <table>
       <thead>
         <tr>
           <th>Name</th>
-          <th>Age</th>
+          <th>Description</th>
           <th>Address</th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="(user) in filtredUsers" :key="user.id">
-          <td><input v-model="user.name" :disabled="user.isDisable"/></td>
-          <td><input v-model="user.age" :disabled="user.isDisable"/></td>
-          <td><input v-model="user.address" :disabled="user.isDisable"/></td>
-          <td><button v-show="user.isDisable" @click="change(user.id)">Change</button></td>
-          <td><button v-show="!user.isDisable" @click="save(user.id)">Save</button></td>
+        <tr v-for="user in filtredUsers" :key="user.id">
+          <td><input v-model="user.name" :disabled="user.isDisable" /></td>
+          <td><input v-model="user.description" :disabled="user.isDisable" /></td>
+          <td><input v-model="user.address" :disabled="user.isDisable" /></td>
+          <td>
+            <button v-show="user.isDisable" @click="change(user.id)">
+              Change
+            </button>
+          </td>
+          <td>
+            <button v-show="!user.isDisable" @click="save(user.id)">
+              Save
+            </button>
+          </td>
           <td><button @click="remove(user.id)">Delete</button></td>
         </tr>
         <tr>
-          <td><input v-model="newUserName" type="text"></td>
-          <td><input v-model="newUserAge" type="text"></td>
-          <td><input v-model="newUserAddress" type="text"></td>
+          <td><input v-model="newUserName" type="text" /></td>
+          <td><input v-model="newUserDescription" type="text" /></td>
+          <td><input v-model="newUserAddress" type="text" /></td>
           <td><button @click="add">Add Item</button></td>
         </tr>
       </tbody>
@@ -40,7 +48,7 @@ export default {
   data () {
     return {
       newUserName: '',
-      newUserAge: '',
+      newUserDescription: '',
       newUserAddress: '',
       findUser: ''
     }
@@ -71,11 +79,11 @@ export default {
     add: function () {
       this.$store.dispatch('addUser', {
         name: this.newUserName,
-        age: this.newUserAge,
+        description: this.newUserDescription,
         address: this.newUserAddress
       })
       this.newUserName = ''
-      this.newUserAge = ''
+      this.newUserDescription = ''
       this.newUserAddress = ''
     },
     remove: function (id) {
