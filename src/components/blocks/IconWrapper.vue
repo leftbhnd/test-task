d5ac6eaee5a0e895edb8df377d6c5758c2d27987
d.laletin <d.laletin@promo-bot.ru>
2021-10-25 16:18:24 +0500

    feat: добавлен компонент для отрисовки svg

---
+++ src/components/blocks/IconWrapper.vue
@@ -0,0 +1,13 @@
+<template>
+  <img :src="src" :style="{ transform: 'rotate(' + degree + 'deg)' }" />
+</template>
+
+<script lang="js">
+export default {
+  name: 'Icon',
+  props: {
+    src: { type: String, require: true },
+    degree: { type: String, default: '0' }
+  }
+}
+</script>
