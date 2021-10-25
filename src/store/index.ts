e13bf2783b3b3bee4d81908aadd82417aa93eab5
d.laletin <d.laletin@promo-bot.ru>
2021-10-25 16:19:25 +0500

    test: добавлен vuex для отладки

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,93 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { IUser } from '@/store/types'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    users: [
+      {
+        name: 'Дима',
+        phone: 228,
+        email: 'd@p.ru',
+        registration: '01.11.1999',
+        code: 1,
+        city: 'Perm'
+      },
+      {
+        name: 'Дима',
+        phone: 228,
+        email: 'd@p.ru',
+        registration: '01.11.1999',
+        code: 2,
+        city: 'Perm'
+      },
+      {
+        name: 'Дима',
+        phone: 228,
+        email: 'd@p.ru',
+        registration: '01.11.1999',
+        code: 3,
+        city: 'Perm'
+      },
+      {
+        name: 'Дима',
+        phone: 228,
+        email: 'd@p.ru',
+        registration: '01.11.1999',
+        code: 4,
+        city: 'Perm'
+      },
+      {
+        name: 'Дима',
+        phone: 228,
+        email: 'd@p.ru',
+        registration: '01.11.1999',
+        code: 5,
+        city: 'Perm'
+      },
+      {
+        name: 'Дима',
+        phone: 228,
+        email: 'd@p.ru',
+        registration: '01.11.1999',
+        code: 6,
+        city: 'Perm'
+      },
+      {
+        name: 'Дима',
+        phone: 228,
+        email: 'd@p.ru',
+        registration: '01.11.1999',
+        code: 7,
+        city: 'Perm'
+      },
+      {
+        name: 'Дима',
+        phone: 228,
+        email: 'd@p.ru',
+        registration: '01.11.1999',
+        code: 8,
+        city: 'Perm'
+      },
+      {
+        name: 'Дима',
+        phone: 228,
+        email: 'd@p.ru',
+        registration: '01.11.1999',
+        code: 12,
+        city: 'Perm'
+      }
+    ] as IUser[]
   },
   mutations: {
   },
   actions: {
   },
-  modules: {
+  getters: {
+    getUsers (state): IUser[] {
+      return state.users
+    }
   }
 })
---
+++ src/store/types.ts
@@ -0,0 +1,8 @@
+export interface IUser {
+  name: string
+  phone: number
+  email: string
+  registration: string
+  code: number
+  city: string
+}
