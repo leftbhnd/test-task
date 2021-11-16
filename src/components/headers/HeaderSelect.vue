2d90a85e7c91286ea86a3e8fbaa80b188653867e
d.laletin <d.laletin@promo-bot.ru>
2021-11-16 10:18:48 +0500

    feat: добавлен header выбора записей

---
+++ src/components/headers/HeaderSelect.vue
@@ -0,0 +1,107 @@
+<template>
+  <div>
+    <HeaderEdit v-if="getEditState" :type="type" />
+    <header class="header-select container" v-else>
+      <div class="header-select__title">
+        <span>Выделено: {{ usersCount }}</span>
+      </div>
+      <div class="header-select__control control">
+        <button class="header-select__edit" @click="editUsers">
+          Редактировать
+        </button>
+        <button class="header-select__delete" @click="deleteUsers">
+          Удалить
+        </button>
+        <button class="header-select__cancel" @click="cancelSelection">
+          Отмена
+        </button>
+      </div>
+    </header>
+  </div>
+</template>
+
+<script lang="ts">
+import { mapGetters, mapActions } from 'vuex'
+export default {
+  name: 'HeaderSelect',
+  data (): { type: string } {
+    return {
+      type: 'edit'
+    }
+  },
+  computed: {
+    ...mapGetters(['getEditState', 'getSelectedUsers']),
+    usersCount (): number {
+      return this.getSelectedUsers.length
+    }
+  },
+  methods: {
+    ...mapActions(['cancelSelection']),
+    editUsers (): void {
+      console.log(123)
+    },
+    deleteUsers (): void {
+      console.log(321)
+    },
+    cancelSelection (): void {
+      this.$store.dispatch('cancelSelection')
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.header-select {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  width: 100%;
+  height: 48px;
+
+  color: #ffffff;
+  background-color: #1f232e;
+  &__title {
+    flex: 0 0 125px;
+
+    padding: 16px;
+  }
+  &__control {
+    display: flex;
+    flex: 0 0 372px;
+  }
+  &__edit {
+    flex: 0 0 170px;
+  }
+  &__edit:hover {
+    background-color: #3e424b;
+  }
+  &__delete {
+    flex: 0 0 119px;
+  }
+  &__delete:hover {
+    background-color: #3e424b;
+  }
+  &__cancel {
+    flex: 0 0 83px;
+  }
+  &__cancel:hover {
+    background-color: #3e424b;
+  }
+  &__edit,
+  &__deletem &__cancel {
+    width: 100%;
+    height: 48px;
+    padding: 16px;
+
+    color: #ffffff;
+    background-color: #1f232e;
+
+    font-family: IBM Plex Mono;
+    font-size: 14px;
+    font-weight: normal;
+    font-style: normal;
+    line-height: 16px;
+  }
+}
+</style>
