e10358d238aa94a26f2fb3134fae3c360080f45a
d.laletin <d.laletin@promo-bot.ru>
2021-11-22 12:29:54 +0500

    feat: добавлена заглушка для get метода

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
+import { get } from '@/api/get'
 
 Vue.config.productionTip = false
 
 new Vue({
   store,
-  render: h => h(App)
+  mounted () {
+    get()
+  },
+  render: (h) => h(App)
 }).$mount('#app')
