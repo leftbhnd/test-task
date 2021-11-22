ee183ab9037fd02b51cc5842c7bba90690198cc4
d.laletin <d.laletin@promo-bot.ru>
2021-11-22 12:34:34 +0500

    fix: рефакторинг функции, удалена лишняя функция

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -1,20 +1,7 @@
 import { IUser } from '@/store/types'
 
-export function isElementInArray (id: number, list: IUser[]): boolean {
+export function isUserInArray (user: IUser, list: IUser[]): boolean {
   return list.some(el => {
-    return el.id === id
+    return el.id === user.id
   })
-}
-
-export function uniqueUsers (users: IUser[], selected: IUser[]): IUser[] {
-  let index = 0
-  const uniq = users.filter(el => {
-    if (!isElementInArray(el.id, selected)) {
-      el.id = index
-      index++
-      return el
-    }
-  }) as IUser[]
-  console.log(uniq)
-  return uniq
 }
