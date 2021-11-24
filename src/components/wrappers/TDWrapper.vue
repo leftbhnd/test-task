a56b31c9764d175088dea0e8391c7162daccf0e8
d.laletin <d.laletin@promo-bot.ru>
2021-11-24 18:54:00 +0500

    refactor: добавлены пропсы, небольшие улучшения

--- src/components/wrappers/TDWrapper.vue
+++ src/components/wrappers/TDWrapper.vue
@@ -1,50 +1,77 @@
 <template>
-  <td :style="{ flex: '1 1 ' + width }">
+  <td>
     <input
       v-if="editCondition"
       class="table-input"
       type="text"
       v-model="value"
-      :style="dataValid"
+      :style="inputStyle"
     />
-    <span v-else>{{ userData }}</span>
+    <span class="trow__label" :style="textStyle" v-else>{{ userData }}</span>
   </td>
 </template>
 
 <script lang="ts">
-import { mapGetters } from 'vuex'
+import { mapGetters, mapActions } from 'vuex'
 import { Validator } from '@/helpers/validator'
 
 const validator = new Validator()
+
 export default {
   name: 'TDWrapper',
   props: {
-    type: { type: String },
-    userData: { type: String },
-    isSelected: { type: Boolean },
-    width: { type: String }
+    userData: String,
+    // userInfo: { isSelected: boolean, isValid: boolean, id: Number, isEdit: boolean }
+    userInfo: Object,
+    type: String
   },
-  data (): { value: string } {
+  data (): { value: string; inputStyle: string } {
     return {
-      value: this.userData
+      value: this.userData,
+      inputStyle: 'border: 1px solid #4aa6ff;'
     }
   },
   computed: {
     ...mapGetters(['getEditEvent']),
     editCondition (): boolean {
-      return this.getEditEvent && this.isSelected
+      return this.getEditEvent && this.userInfo.isSelected
     },
-    dataValid (): string {
-      if (validator.isValid({ type: this.type, value: this.data })) {
-        return 'border: 1px solid #4aa6ff;'
+    isValueValid (): boolean {
+      return validator.isValid({ type: this.type, value: this.value })
+    },
+    textStyle (): string {
+      if (this.userInfo.isEdit) {
+        return 'color: #7b8395;'
       } else {
-        return 'border: 1px solid red;'
+        return 'color: #1f232e;'
       }
     }
+  },
+  methods: {
+    ...mapActions(['validationObserver'])
   },
   watch: {
     value (): void {
       this.$emit('value', this.value)
+      if (!this.isValueValid) {
+        this.$store.dispatch('validationObserver', {
+          id: this.userInfo.id,
+          type: this.type,
+          value: this.value
+        })
+      }
+    },
+    getEditEvent (): void {
+      if (!this.editCondition) {
+        this.value = this.userData
+      }
+    },
+    isValueValid (): void {
+      if (this.isValueValid) {
+        this.inputStyle = 'border: 1px solid #4aa6ff;'
+      } else {
+        this.inputStyle = 'border: 1px solid red;'
+      }
     }
   }
 }
